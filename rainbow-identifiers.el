19fbfded1baa98d12335f26f6d7b20e5ae44ce2e
Fanael Linithien <fanael4@gmail.com>
2014-11-02 16:24:39 +0100

    Release version 0.2.2.

--- rainbow-identifiers.el
+++ rainbow-identifiers.el
@@ -2,7 +2,7 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/rainbow-identifiers
-;; Version: 0.2.1
+;; Version: 0.2.2
 ;; Package-Requires: ((emacs "24"))
 
 ;; This file is NOT part of GNU Emacs.
